2f9227c4f96ac15340ba0d23a3c40945863266f9
wk2323-sudo <wk2323@nyu.edu>
2026-03-30 23:49:45 -0400

    Midterm_finalversion

--- Therapeutic_pirate_2026_03_14_00_33_42/sketch.js
+++ Therapeutic_pirate_2026_03_14_00_33_42/sketch.js
@@ -1,10 +1,19 @@
 let particles = [];
 let state = 0;
 let stateTimer = 0;
-const sceneDuration = 450; 
+
+
+const sceneDurations = [800, 1020, 900, 1200, 800];
+
+
+let bonds = [];
+const SNAP_DIST = 72;  
+const BOND_LIFE = 90;  
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  colorMode(RGB, 255, 255, 255, 255);
+
   for (let i = 0; i < 180; i++) {
     particles.push(new Particle(random(width), random(height)));
   }
@@ -12,86 +21,177 @@ function setup() {
 
 function draw() {
   handleBackground(state);
-  
   updateStateMachine();
-  displaySceneLabel(); 
 
+  
+  if (state === 3) {
+    updateBonds();
+    drawBonds();
+  } else {
+    bonds = [];
+  }
+
+  
   for (let i = 0; i < particles.length; i++) {
     let p = particles[i];
-    
     p.applyBehaviors(state);
-    
+
     if (state === 3) {
       for (let j = i + 1; j < particles.length; j++) {
         p.interact(particles[j]);
       }
     }
-    
+
     p.update();
     p.display(state);
   }
-  
-  if (state === 1) {
-    drawFieldLines(mouseX, mouseY, 200);
-  }
+
+  if (state === 1) drawFieldLines(mouseX, mouseY, 220);
+
+  displaySceneLabel();
 }
+
 
 function updateStateMachine() {
   stateTimer++;
-  if (stateTimer > sceneDuration) {
-    state = (state + 1) % 5; 
+  if (stateTimer > sceneDurations[state]) {
+    state = (state + 1) % 5;
     stateTimer = 0;
+    bonds = [];
   }
 }
 
+
 function handleBackground(s) {
-  if (s === 2) background(15, 20, 40, 40); 
-  else if (s === 3) background(40, 15, 15, 40); 
-  else if (s === 4) background(255, 30); 
-  else background(10, 15, 30, 40); 
+  if (s === 2)      background(15, 20, 40, 40);
+  else if (s === 3) background(40, 15, 15, 40);
+  else if (s === 4) background(255, 255, 255, 30);
+  else              background(10, 15, 30, 40);
 }
+
 
 function drawFieldLines(x, y, radius) {
   push();
   noFill();
-  stroke(255, 70);
-  let pulse = sin(frameCount * 0.1) * 20;
-  for (let r = 50; r < radius; r += 40) {
-    ellipse(x, y, r + pulse, r + pulse);
+  let pulse   = sin(frameCount * 0.07) * 22;
+  let numRings = 5;
+
+  for (let r = 0; r < numRings; r++) {
+    let rr = 45 + r * 38 + pulse;
+   
+    let a  = map(r, 0, numRings - 1, 170, 25);
+    let sw = map(r, 0, numRings - 1, 1.8, 0.3);
+    stroke(180, 220, 255, a);
+    strokeWeight(sw);
+    ellipse(x, y, rr, rr);
+  }
+
+
+  for (let a = 0; a < TWO_PI; a += TWO_PI / 12) {
+    let x2 = x + cos(a) * (radius + pulse * 0.4);
+    let y2 = y + sin(a) * (radius + pulse * 0.4);
+    stroke(180, 220, 255, 30);
+    strokeWeight(0.5);
+    line(x, y, x2, y2);
+  }
+  pop();
+}
+
+function updateBonds() {
+  for (let i = 0; i < particles.length; i++) {
+    for (let j = i + 1; j < particles.length; j++) {
+      let a = particles[i];
+      let b = particles[j];
+      if (a.charge === b.charge) continue; 
+
+      let d = dist(a.pos.x, a.pos.y, b.pos.x, b.pos.y);
+      if (d < SNAP_DIST) {
+        let existing = bonds.find(
+          bd => (bd.a === a && bd.b === b) || (bd.a === b && bd.b === a)
+        );
+        if (!existing) {
+          bonds.push({ a, b, life: BOND_LIFE, maxLife: BOND_LIFE });
+        } else {
+          existing.life = min(existing.life + 4, BOND_LIFE); 
+        }
+        
+        let pull = p5.Vector.sub(b.pos, a.pos).normalize().mult(0.38);
+        a.applyForce(pull);
+        b.applyForce(pull.copy().mult(-1));
+      }
+    }
+  }
+
+  
+  for (let i = bonds.length - 1; i >= 0; i--) {
+    bonds[i].life--;
+    if (bonds[i].life <= 0) bonds.splice(i, 1);
+  }
+}
+
+function drawBonds() {
+  push();
+  noFill();
+  for (let bd of bonds) {
+    let t  = bd.life / bd.maxLife; 
+    let sw = map(t, 0, 1, 0.2, 2.6);
+    let a  = map(t, 0, 1, 0, 210);
+
+    
+    stroke(180, 80, 200, a);
+    strokeWeight(sw);
+    line(bd.a.pos.x, bd.a.pos.y, bd.b.pos.x, bd.b.pos.y);
+
+    
+    let mx = (bd.a.pos.x + bd.b.pos.x) * 0.5;
+    let my = (bd.a.pos.y + bd.b.pos.y) * 0.5;
+    let haloR = map(t, 0, 1, 0, 10) + sin(frameCount * 0.18) * 3;
+    stroke(220, 140, 255, a * 0.5);
+    strokeWeight(sw * 2.5);
+    point(mx, my);
+    noFill();
+    strokeWeight(0.6);
+    stroke(220, 140, 255, a * 0.25);
+    ellipse(mx, my, haloR, haloR);
   }
   pop();
 }
 
 class Particle {
   constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.vel = p5.Vector.random2D();
-    this.acc = createVector(0, 0);
+    this.pos      = createVector(x, y);
+    this.vel      = p5.Vector.random2D();
+    this.acc      = createVector(0, 0);
     this.maxSpeed = random(2, 5);
     this.maxForce = 0.2;
-    this.charge = random([-1, 1]); 
-    this.size = random(4, 10);
-    this.history = []; 
+    this.charge   = random([-1, 1]); 
+    this.size     = random(4, 10);
+    this.history  = [];
+
+    
+    this.forceMag = 0;
   }
 
   applyBehaviors(s) {
-    if (s === 0) { 
+    if (s === 0) {
       let drift = p5.Vector.random2D().mult(0.5);
       this.applyForce(drift);
-    } 
-    else if (s === 1) { 
+    }
+    else if (s === 1) {
       let target = createVector(mouseX, mouseY);
       this.applyForce(this.seek(target).mult(1.2));
     }
-    else if (s === 2) { 
-      let flow = createVector(cos(this.pos.y * 0.01), sin(this.pos.x * 0.01));
+    else if (s === 2) {
+      let flow = createVector(
+        cos(this.pos.y * 0.01),
+        sin(this.pos.x * 0.01)
+      );
       this.applyForce(flow.mult(1.5));
     }
-    else if (s === 4) { 
-      let center = createVector(width/2, height/2);
+    else if (s === 4) {
+      let center = createVector(width / 2, height / 2);
       this.applyForce(this.seek(center).mult(2.5));
     }
-    
   }
 
   seek(target) {
@@ -107,13 +207,15 @@ class Particle {
     if (d < 100 && d > 0) {
       let force = p5.Vector.sub(this.pos, other.pos);
       force.normalize();
-      
+
       if (this.charge === other.charge) {
-        force.mult(1.2 / (d * 0.1)); 
+        force.mult(1.2 / (d * 0.1));   
       } else {
-        force.mult(-1.8 / (d * 0.1)); 
+        force.mult(-1.8 / (d * 0.1));  
       }
+
       this.applyForce(force);
+      this.forceMag = force.mag(); 
     }
   }
 
@@ -127,42 +229,76 @@ class Particle {
     this.pos.add(this.vel);
     this.acc.mult(0);
     this.edges();
-    
+
     this.history.push(this.pos.copy());
-    if (this.history.length > 12) this.history.shift();
+    if (this.history.length > 14) this.history.shift();
+
+    this.forceMag *= 0.88; 
   }
 
   display(s) {
-    let col = this.charge > 0 ? color(0, 150, 255) : color(255, 50, 100);
-    if (s === 4) col = color(0, 50); 
     
+    let r, g, b;
+    if (this.charge > 0) { r = 30;  g = 160; b = 255; } 
+    else                  { r = 255; g = 50;  b = 100; } 
+    if (s === 4)          { r = 20;  g = 20;  b = 30;  } 
+
+    let speed     = this.vel.mag();
+    let intensity = constrain(speed / this.maxSpeed, 0, 1);
+    let forceBoost = constrain(this.forceMag * 18, 0, 100);
+
+   
+    let alpha = constrain(120 + intensity * 80 + forceBoost, 60, 255);
+
+  
+    let sw = map(intensity, 0, 1, 0.6, 2.2) +
+             map(this.forceMag, 0, 5, 0, 1.5);
+
     push();
     translate(this.pos.x, this.pos.y);
     rotate(this.vel.heading());
-    
+
+  
     noFill();
-    stroke(col);
-    strokeWeight(1);
+    stroke(r, g, b, alpha * 0.4);
+    strokeWeight(sw * 0.7);
     beginShape();
     for (let i = 0; i < this.history.length; i++) {
       let offset = p5.Vector.sub(this.history[i], this.pos);
-      vertex(offset.x, offset.y); 
+      vertex(offset.x, offset.y);
     }
     endShape();
 
-    fill(col);
+    // Body
+    fill(r, g, b, alpha);
     noStroke();
-    rect(-this.size, -1, this.size * 2, 2);
+    let bodyLen = this.size * (1 + intensity * 0.6);
+    rect(-bodyLen, -1.2, bodyLen * 2, 2.4);
+
+    
+    let dotR = this.size * 0.55 + map(this.forceMag, 0, 5, 0, 3);
+    if (this.charge > 0) {
+      fill(r, g, b, alpha);
+      noStroke();
+      ellipse(bodyLen, 0, dotR, dotR);
+    } else {
+      noFill();
+      stroke(r, g, b, alpha);
+      strokeWeight(sw);
+      ellipse(bodyLen, 0, dotR, dotR);
+    }
+
     pop();
   }
 
   edges() {
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.x > width)  this.pos.x = 0;
+    if (this.pos.x < 0)      this.pos.x = width;
     if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y < 0) this.pos.y = height;
+    if (this.pos.y < 0)      this.pos.y = height;
   }
 }
+
 
 function mousePressed() {
   for (let p of particles) {
@@ -172,9 +308,14 @@ function mousePressed() {
   }
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+
 function displaySceneLabel() {
-  let barWidth = map(stateTimer, 0, sceneDuration, 0, width);
+  let barWidth = map(stateTimer, 0, sceneDurations[state], 0, width);
   noStroke();
-  fill(255, 80);
+  fill(255, 255, 255, 80);
   rect(0, height - 4, barWidth, 4);
 }
